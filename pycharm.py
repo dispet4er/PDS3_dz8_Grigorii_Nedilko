4fd7efe11eb008ced0ccfd80deeb25fea488563b
Grigorii Nedilko <nedilko.gp@gmail.com>
2022-11-08 15:30:35 +0200

    Commit1 test1 pycharm

--- pycharm.py
+++ pycharm.py
@@ -2,3 +2,6 @@ def pycharm_function():
     print("Commit1 master pycharm")
     print("Commit2 master pycharm")
     print("Commit3 master pycharm")
+
+def pycharm_function():
+    print("Commit1 test1 pycharm")
